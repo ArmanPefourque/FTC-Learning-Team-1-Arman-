5b44647824d5d9e097ba235df86e67dd4cadec13
AdvayRoongta <roongtaadvay@gmail.com>
2025-02-10 16:24:50 -0800

    FIRST OPMODE!

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/Teleop.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/Teleop.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+//TEST
 @TeleOp(name="Teleop", group="Pushbot")
 public class Teleop extends OpMode {
 
