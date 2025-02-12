0b80e0229ddfaa1fcb3ec5a11d82931fe38a902f
PenguinWizard <155591091+PenguinWizard@users.noreply.github.com>
2025-02-11 16:43:36 -0800

    Make testing OpMode - Success!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopRizz.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopRizz.java
@@ -1,13 +1,12 @@
 package org.firstinspires.ftc.teamcode; //our code
-import com.qualcomm.robotcore.util.ElapsedTime;
 import java;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 //TEST
-@TeleOp(name="Teleop", group="Pushbot")
-public class Teleop extends OpMode {
+@TeleOp(name="TeleopRizz", group="Pushbot")
+public class TeleopRizz extends OpMode {
 
     private DcMotor frontRight= null; //jaZDVVZDZDZSVVZDZDVva sucks
     private DcMotor frontLeft= null; //python lowk better
@@ -18,19 +17,20 @@ public class Teleop extends OpMode {
 
     @Override
     public void init(){
-        robot.init(hardwareMap); //connects to all hardware components
+//        robot.init(hardwareMap); //connects to all hardware components
         telemetry.addData("travis:", "fein fein fein fein");
+        frontLeft =
 
     }
     @Override //repeats after PLAY, BEFORE STOP
     public void loop(){
 
-        robot.frontLeft.setPower(0.5);
-        robot.backLeft.setPower(0.5);
-        robot.frontRight.setPower(0.5);
-        robot.backRight.setPower(0.5);
-        telemetry.addLine(String.format("It should be moving");
+        robot.frontLeft.setPower(0.1);
+        robot.backLeft.setPower(0.1);
+        robot.frontRight.setPower(0.1);
+        robot.backRight.setPower(0.1);
+        telemetry.addLine(String.format("It should be moving"));
 
-        Thread.sleep(2000)//Francisco only wants to see the robot move
+//        Thread.sleep(2000)//Francisco only wants to see the robot move
     }
 }
